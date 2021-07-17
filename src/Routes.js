c09cbe938956360895c9fdb384f06d14aac87b40
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2021-07-17 13:02:08 -0300

    feat: rota profissao

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import swaggerUI from 'swagger-ui-express';
 
 import * as specs from './config/swagger';
 
-import ping_route from './app/routes/ping';
+import { ping_route, profession_route } from './app/routes';
 
 const routes = new Router();
 
 routes.use('/documentation', swaggerUI.serve, swaggerUI.setup(specs.default));
 
 routes.use(ping_route);
+
+routes.use('/profession', profession_route);
 
 export default routes;
